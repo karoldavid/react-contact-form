8833ad348d02658101e57f7b7988ab8dd54360a7
karoldavid <k.zysk@zoho.com>
2018-01-12 18:19:11 +0100

    feat: set required fields

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -9,14 +9,14 @@ import * as actions from "../actions";
 const FIELDS = [
 	{ name: "firstName", label: "First Name", required: true },
 	{ name: "lastName", label: "Last Name", required: true },
-	{ name: "company", label: "Company Name", required: false },
-	{ name: "email", label: "Email Address", required: false },
+	{ name: "company", label: "Company Name", required: true },
+	{ name: "email", label: "Email Address", required: true },
 	{ name: "number", label: "Phone Number", required: false },
 	{ name: "skype", label: "Skype ID", required: false },
 	{
 		name: "projectDescription",
 		label: "Project Description",
-		required: false
+		required: true
 	}
 ];
 
